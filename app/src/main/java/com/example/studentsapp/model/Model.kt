a9a4d1a1738008b54ea17b2a65dc922b5c1f2303
guyYablonka <guyyablonka@gmail.com>
2026-01-18 21:24:51 +0200

    add models and data layer

---
+++ app/src/main/java/com/example/studentsapp/model/Model.kt
@@ -0,0 +1,40 @@
+package com.example.studentsapp.model
+
+class Model private constructor() {
+
+    private val students = mutableListOf<Student>()
+
+    companion object {
+        val instance: Model by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+            Model()
+        }
+    }
+
+    init {
+        // Add some initial data for testing
+        for (i in 0..15) {
+            students.add(Student("Student $i", "$i", "050-123456$i", "Street $i", false))
+        }
+    }
+
+    fun getAllStudents(): List<Student> = students
+
+    fun addStudent(student: Student) {
+        students.add(student)
+    }
+
+    fun getStudentById(id: String): Student? {
+        return students.find { it.id == id }
+    }
+
+    fun deleteStudent(id: String) {
+        students.removeAll { it.id == id }
+    }
+
+    fun updateStudent(oldId: String, updatedStudent: Student) {
+        val index = students.indexOfFirst { it.id == oldId }
+        if (index != -1) {
+            students[index] = updatedStudent
+        }
+    }
+}
